7e769d668c7c479f9d5a7ad6e5c9b1a9bce07f25
Lucas <Rodriguesdesouzal28@gmail.com>
2022-12-15 09:41:42 -0300

    feat: innitial configs and connection db

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from 'express0';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT;
+app.listen(port,()=> console.log(`Running in port ${port}`));

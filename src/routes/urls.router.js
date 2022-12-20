f29ab33490f6b7d73e1e150d34bd35fe3eaacc9c
Lucas <Rodriguesdesouzal28@gmail.com>
2022-12-20 10:19:30 -0300

    feat: route get by shortUrl

--- src/routes/urls.router.js
+++ src/routes/urls.router.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import validationPostShortenUrl from '../middlewares/urls.middleware.js';
-import {postShortenUrl,getUrlById} from '../controllers/urls.controller.js';
+import {postShortenUrl,getUrlById,getShortUrl} from '../controllers/urls.controller.js';
 const router = Router();
 
 router.post("/urls/shorten",validationPostShortenUrl,postShortenUrl)
 router.get("/urls/:id",getUrlById)
+router.get("/urls/open/:shortUrl",getShortUrl)
 
 export default router;

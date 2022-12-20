a1fd881d7ea611d42090d42791c9ef2e5b449bf3
Lucas <Rodriguesdesouzal28@gmail.com>
2022-12-20 09:27:46 -0300

    feat: route get urls By Id

--- src/controllers/urls.controller.js
+++ src/controllers/urls.controller.js
@@ -1,6 +1,6 @@
 import { nanoid } from "nanoid";
 import connection from '../database/db.js';
-export default async function postShortenUrl(req, res) {
+export async function postShortenUrl(req, res) {
   const user = res.locals.user;
   const { url } = req.body;
   const shortUrl = nanoid();
@@ -9,6 +9,26 @@ export default async function postShortenUrl(req, res) {
     await connection.query(`INSERT INTO urls (url, "shortUrl", user_id) VALUES ($1, $2, $3);`,
       [url, shortUrl, user.id]);
     res.status(201).send({ message: "Url shortend.", shortUrl: shortUrl });
+    return;
+  } catch (err) {
+    res.sendStatus(500);
+  }
+}
+
+export async function getUrlById(req, res) {
+  const { id } = req.params;
+  try {
+    const url = (await connection.query("SELECT * FROM urls WHERE id=$1;", [id])).rows[0]
+
+    if (!url) {
+      res.status(404).send({ message: "Esse Id de url não existe." });
+      return;
+    }
+    res.status(200).send({
+      id: url.id,
+      shortUrl: url.shortUrl,
+      url: url.url,
+    });
     return;
   } catch (err) {
     res.sendStatus(500);
--- src/routes/urls.router.js
+++ src/routes/urls.router.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import validationPostShortenUrl from '../middlewares/urls.middleware.js';
-import postShortenUrl from '../controllers/urls.controller.js';
+import {postShortenUrl,getUrlById} from '../controllers/urls.controller.js';
 const router = Router();
 
 router.post("/urls/shorten",validationPostShortenUrl,postShortenUrl)
-
+router.get("/urls/:id",getUrlById)
 
 export default router;

b3d52c0c05fbbf25408f04abe35dff700543c028
Lucas <Rodriguesdesouzal28@gmail.com>
2022-12-23 17:39:43 -0300

    fix db

--- src/database/db.js
+++ src/database/db.js
@@ -1,13 +1,28 @@
-import pkg from 'pg';
+/* import pkg from 'pg';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const { Pool } = pkg;
 
 const connection = new Pool({
-    connectionString: process.env.DATABASE_URL, 
+    connectionString: process.env.DATABASE_URL,
     ssl: true,
-    
-})
+  });
+  
+export default connection; */
 
-export default connection;
+import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
+const { Pool } = pg;
+
+const configDatabase = {
+  connectionString: process.env.DATABASE_URL,
+};
+
+if (process.env.MODE === "prod") configDatabase.ssl = true;
+
+ const connection = new Pool(configDatabase);
+
+ export default connection;
